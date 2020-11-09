061608c233c060799a181c03af914c1cc37e756f
bl4ckmount <nicknail.sup@gmail.com>
2020-11-09 19:48:02 +0400

    Add JSON parser, page loader + some CSS improvements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// data fetching func
+window.onload = function() {
+    fetch('https://hook.integromat.com/p6yr2nhayn6pjhw3qzb0mexhw0jcl7ub')
+    .then((resp) => resp.json())
+    .then(function(data) {
+        return data.map(function(prtc) {
+            let block = document.createElement('div'),
+                link = document.createElement('a'),
+                name = document.createElement('p'),
+                timestamp = document.createElement('p');
+
+            block.className = 'rec-list__item';
+            link.setAttribute('href', prtc.webViewLink);
+            name.textContent = prtc.name;
+            timestamp.textContent = new Date(prtc.modifiedTime).toLocaleString('ru', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            });
+
+            document.querySelector('.rec-list').appendChild(block);
+            block.appendChild(link);
+            link.appendChild(name);
+            block.appendChild(timestamp);
+        })
+    })
+}
+
 // header opacity
 window.addEventListener('scroll', function() {
     if (window.innerWidth > 425) {
@@ -22,4 +50,14 @@ document.querySelector('#search').addEventListener('input', function() {
             }
         }
     }
+});
+
+// turning off the loader
+document.addEventListener("DOMNodeInserted", function() { 
+    document.querySelector('.header').style.display = 'flex';
+    document.querySelector('.loader').style.animation = '.65s ease-in fade';
+    setTimeout(function() {
+        document.querySelector('.loader').style.opacity = 0;
+        document.querySelector('.loader').style.display = 'none';
+    }, 650);
 });
