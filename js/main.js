8a25b5e310d4557a9b4a3fdf072eb7e99e6b749d
bl4ckmount <nicknail.sup@gmail.com>
2020-11-12 18:51:15 +0400

    Add website icon, remove page loader and minor bug fixes

--- js/main.js
+++ js/main.js
@@ -1,63 +1,62 @@
 // data fetching func
 window.onload = function() {
-    fetch('https://hook.integromat.com/p6yr2nhayn6pjhw3qzb0mexhw0jcl7ub')
+    fetch("https://hook.integromat.com/5y4awjulioas6l7nl7xnoqn5ot471nx6")
     .then((resp) => resp.json())
     .then(function(data) {
         return data.map(function(prtc) {
-            let block = document.createElement('div'),
-                link = document.createElement('a'),
-                name = document.createElement('p'),
-                timestamp = document.createElement('p');
+            let block = document.createElement("div"),
+                link = document.createElement("a"),
+                name = document.createElement("p"),
+                timestamp = document.createElement("p");
 
-            block.className = 'rec-list__item';
-            link.setAttribute('href', prtc.webViewLink);
+            block.className = "rec-list__item";
+            link.setAttribute("href", prtc.webViewLink);
             name.textContent = prtc.name;
-            timestamp.textContent = new Date(prtc.modifiedTime).toLocaleString('ru', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric'
+            timestamp.textContent = new Date(prtc.modifiedTime).toLocaleString("ru", {
+                day: "numeric",
+                month: "long",
+                year: "numeric"
             });
 
-            document.querySelector('.rec-list').appendChild(block);
+            document.querySelector(".rec-list").appendChild(block);
             block.appendChild(link);
             link.appendChild(name);
             block.appendChild(timestamp);
         })
     })
+    .catch(function() {
+        document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[0].style.display = "none";
+        
+        document.querySelector(".rec-list").style.display = "flex";
+        document.querySelector(".rec-list").style.justifyContent = "center";
+
+        document.querySelector(".rec-list__error").style.animation = "1.5s fade reverse";
+        document.querySelector(".rec-list__error").style.display = "block";
+    });
 }
 
 // header opacity
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function() {
     if (window.innerWidth > 425) {
         if (window.pageYOffset >= 50) {
-            document.querySelector('.header').style.background = '#1e1e1e';
+            document.querySelector(".header").style.background = "#1e1e1e";
         } else {
-            document.querySelector('.header').style.background = 'none';
+            document.querySelector(".header").style.background = "none";
         }
     }
 });
 
 // searching func
-document.querySelector('#search').addEventListener('input', function() {
-    for (i = 0; i < document.querySelector('.rec-list').getElementsByClassName('rec-list__item').length; i++) {
-        if (i != 0) {
-            item = document.querySelector('.rec-list').getElementsByClassName('rec-list__item')[i];
+document.querySelector("#search").addEventListener("input", function() {
+    for (i = 0; i < document.querySelector(".rec-list").getElementsByClassName("rec-list__item").length; i++) {
+        if (i > 0) {
+            item = document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[i];
             txtValue = item.firstChild.textContent || item.firstChild.innerText;
-            if (txtValue.toUpperCase().indexOf(document.querySelector('#search').value.toUpperCase()) > -1) {
-                item.style.display = 'flex';
+            if (txtValue.toUpperCase().indexOf(document.querySelector("#search").value.toUpperCase()) > -1) {
+                item.style.display = "flex";
             } else {
-                item.style.display = 'none';
+                item.style.display = "none";
             }
         }
     }
-});
-
-// turning off the loader
-document.addEventListener("DOMNodeInserted", function() { 
-    document.querySelector('.header').style.display = 'flex';
-    document.querySelector('.loader').style.animation = '1.5s ease-in fade';
-    setTimeout(function() {
-        document.querySelector('.loader').style.opacity = 0;
-        document.querySelector('.loader').style.display = 'none';
-    }, 1500);
 });
