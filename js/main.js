123ed8a1ea80be6a8a99908199116c9621fff7ce
bl4ckmount <nicknail.sup@gmail.com>
2020-11-27 12:28:04 +0400

    Add archive page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-// data fetching func
+var clicked = 0;
+
+// refresh delay initializing
+var refresh = setInterval(Refresh, (document.title == "Выбор протокола") ? 60000 : 15000);
+
+// webhook urls for both pages
+const hooks = [
+    "https://hook.integromat.com/5y4awjulioas6l7nl7xnoqn5ot471nx6", 
+    "https://hook.integromat.com/fhgddqrvj572ve4kot9rl01mr9fbwl4b"
+];
+
+// a bit of "on load cfg"
 window.onload = function() {
-    fetch("https://hook.integromat.com/5y4awjulioas6l7nl7xnoqn5ot471nx6")
+    document.querySelector(".header").style.background = "none";
+    Fetch();
+}
+
+// json fetching func
+function Fetch() {
+    fetch((document.title == "Выбор протокола") ? hooks[0] : hooks[1])
     .then((resp) => resp.json())
     .then(function(data) {
         return data.map(function(prtc) {
@@ -12,11 +29,21 @@ window.onload = function() {
             block.className = "rec-list__item";
             link.setAttribute("href", prtc.webViewLink);
             name.textContent = prtc.name;
-            timestamp.textContent = new Date(prtc.modifiedTime).toLocaleString("ru", {
-                day: "numeric",
-                month: "long",
-                year: "numeric"
-            });
+            if (document.title == "Выбор протокола") {
+                timestamp.textContent = new Date(prtc.modifiedTime).toLocaleString("ru", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric"
+                })
+            } else {
+                timestamp.textContent = new Date(prtc.createdTime).toLocaleString("ru", {
+                    hour: "numeric",
+                    minute: "numeric",
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric"
+                })
+            }
 
             document.querySelector(".rec-list").appendChild(block);
             block.appendChild(link);
@@ -25,18 +52,39 @@ window.onload = function() {
         })
     })
     .catch(function() {
-        document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[0].style.display = "none";
-        
+        // error message
+        for (i = 0; i < document.querySelector(".rec-list").getElementsByClassName("rec-list__item").length; i++) { document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[i].style.display = "none"; }
         document.querySelector(".rec-list").style.display = "flex";
-        document.querySelector(".rec-list").style.justifyContent = "center";
-
-        document.querySelector(".rec-list__error").style.animation = "1.5s fade reverse";
-        document.querySelector(".rec-list__error").style.display = "block";
+        document.querySelector(".rec-list__message").style.display = "block";
     });
+}
+
+function Refresh() {
+    clearInterval(refresh); refresh = setInterval(Refresh, (document.title == "Выбор протокола") ? 60000 : 15000);
+    for (i = document.querySelector(".rec-list").getElementsByClassName("rec-list__item").length - 1; i != 0; i--) { 
+        document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[i].remove();
+    }
+    Fetch();
+}
+
+// searching func
+function Search() {
+    for (i = 0; i < document.querySelector(".rec-list").getElementsByClassName("rec-list__item").length; i++) {
+        if (i > 0) {
+            let item = document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[i];
+            let txtValue = item.firstChild.textContent || item.firstChild.innerText;
+            if (txtValue.toUpperCase().indexOf(document.querySelector(".search").value.toUpperCase()) > -1) {
+                item.style.display = "flex";
+            } else {
+                item.style.display = "none";
+            }
+        }
+    }
 }
 
 // header opacity
 window.addEventListener("scroll", function() {
+    // this feature turns off on mobile devices
     if (window.innerWidth > 425) {
         if (window.pageYOffset >= 50) {
             document.querySelector(".header").style.background = "#1e1e1e";
@@ -46,17 +94,13 @@ window.addEventListener("scroll", function() {
     }
 });
 
-// searching func
-document.querySelector("#search").addEventListener("input", function() {
-    for (i = 0; i < document.querySelector(".rec-list").getElementsByClassName("rec-list__item").length; i++) {
-        if (i > 0) {
-            item = document.querySelector(".rec-list").getElementsByClassName("rec-list__item")[i];
-            txtValue = item.firstChild.textContent || item.firstChild.innerText;
-            if (txtValue.toUpperCase().indexOf(document.querySelector("#search").value.toUpperCase()) > -1) {
-                item.style.display = "flex";
-            } else {
-                item.style.display = "none";
-            }
-        }
-    }
-});
+// searching on input
+document.querySelector(".search").addEventListener("input", Search);
+
+// icon rotate on click & refresh
+if (document.title == "Архив") {
+    document.querySelector(".refresh").addEventListener("click", function() {
+        clicked += 1; document.querySelector(".refresh img").style.transform = `rotate(${180 * clicked}deg)`;
+        Refresh();
+    });
+}
